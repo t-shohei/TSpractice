5cd2b9b480fad5b990ae17302b704ab20c989102
cafe-dora <cafedora@icloud.com>
2022-08-05 22:13:04 +0900

    まだ解決していない

--- dist/practice1.js
+++ dist/practice1.js
@@ -1,8 +1,8 @@
 "use strict";
 const data = `
-uhyo,26,1
-John Smith,17,0
-Mary Sue,14,1
+uhyo,26,1,
+John Smith,17,0,
+Mary Sue,14,1,
 `;
 const users = [];
 const lines = data.split("¥n");
@@ -10,15 +10,23 @@ for (const line of lines) {
     if (line === "") {
         continue;
     }
+    // console.log(line)
     const [name, ageString, premiumUserString] = line.split(",");
     const age = Number(ageString);
     const premiumUser = premiumUserString === "1";
+    // console.log(premiumUser)
+    users.push({
+        name,
+        age,
+        premiumUser
+    });
 }
+console.log(users);
 for (const user of users) {
     if (user.premiumUser) {
         console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
     }
     else {
-        console.log(`${user.name}(${user.age}はプレミアムユーザーではありません。)`);
+        console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`);
     }
 }
--- src/practice1.ts
+++ src/practice1.ts
@@ -5,27 +5,34 @@ type User = {
 }
 
 const data: string = `
-uhyo,26,1
-John Smith,17,0
-Mary Sue,14,1
-`;
-
+uhyo,26,1,
+John Smith,17,0,
+Mary Sue,14,1,
+`
 const users: User[] = []
+
 const lines = data.split("¥n")
+
 for (const line of lines) {
   if (line === ""){
     continue
   }
-  const [name, ageString, premiumUserString] = line.split(",")
+  // console.log(line)
+  const [name,ageString,premiumUserString] = line.split(",")
   const age = Number(ageString)
   const premiumUser = premiumUserString === "1"
+  // console.log(premiumUser)
+  users.push({
+    name,
+    age,
+    premiumUser
+  })
 }
-
-
+console.log(users)
 for (const user of users) {
   if (user.premiumUser){
     console.log(`${user.name}(${user.age})はプレミアムユーザーです。`)
   }else{
-    console.log(`${user.name}(${user.age}はプレミアムユーザーではありません。)`)
+    console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`)
   }
 }
