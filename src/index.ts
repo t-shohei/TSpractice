afbdc24398c89cc339e005e9aeeadb9bbc30ba8a
cafe-dora <cafedora@icloud.com>
2023-01-01 22:05:04 +0900

    3.8力試し

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,43 @@
-const message: string = "Hello World"
-console.log(message);
+type User = {
+  name: string;
+  age: number;
+  premiumUser: boolean;
+}
+
+const data: string = `
+uhyo,26,1
+john Smith,17,0
+Mary Sue,14,1
+`;
+
+const users: User[] = [];
+const lines = data.split("\n")
+
+
+for (const line of lines) {
+  if (line === "") {
+    continue;
+  }
+  const [name, ageString, premiumUserString] = line.split(",")
+  const age = Number(ageString)
+  const premiumUser = premiumUserString === "1"
+
+  
+  users.push({
+    name,
+    age,
+    premiumUser
+  });
+}
+
+
+
+
+
+for (const user of users) {
+  if (user.premiumUser) {
+    console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
+  }else{
+    console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`);
+  }
+}
